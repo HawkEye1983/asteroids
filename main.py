d9e3b384c05ee41350785f9031f7224a431a43fb
HawkEye1983 <git@hawkeye.aleeas.com>
2025-05-07 20:25:23 -0600

    Made shots effective by removing asteroids when hit.

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 import pygame
 from constants import *
-from player import Player
+from player import Player, Shot
 from asteroidfield import AsteroidField
 from asteroid import Asteroid
-from player import Shot
 
 
 def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    # Initializing the game
     pygame.init()
+
+    # Setting the screen resolution
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    # Setting up the gaming clock
     clock = pygame.time.Clock()
     dt = 0
-    updatables = pygame.sprite.Group()
-    drawables = pygame.sprite.Group()
-    asteroids = pygame.sprite.Group()
-    shots = pygame.sprite.Group()
+
+    # Creating the groups
+    updatables, drawables, asteroids, shots = pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group(), pygame.sprite.Group()
     AsteroidField.containers = (updatables)
     Asteroid.containers = (asteroids, updatables, drawables)
     Player.containers = (updatables, drawables)
-    Shot.containers = (updatables, drawables)
+    Shot.containers = (updatables, drawables, shots)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
@@ -32,6 +39,11 @@ def main():
         for asteroid in asteroids:
             if not asteroid.collision(player):
                 exit()
+            for shot in shots:
+                if not asteroid.collision(shot):
+                    shot.kill()
+                    asteroid.kill()
+
         for drawable in drawables:
             drawable.draw(screen)
         pygame.display.flip()
